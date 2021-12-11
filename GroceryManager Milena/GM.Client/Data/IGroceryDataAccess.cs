8cf54198c580ff820c36b04de7916a4f97663320
milenadecheva <zlatolina@gmail.com>
2021-12-11 18:56:09 +0200

    Implementation For GroceryManager

---
+++ GroceryManager Milena/GM.Client/Data/IGroceryDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GM.Models;
+
+namespace GM.Client.Data {
+    public interface IGroceryDataAccess {
+        Task<Grocery> AddItemAsync(Grocery item);
+        Task<bool> RemoveItemAsync(Grocery item);
+        Task<IEnumerable<Grocery>> GetAsync(bool showAll, bool sortByDateOfManufactoring, bool sortByDateOfExpiration);
+        Task<Grocery> UpdateAsync(Grocery item);
+    }
+}
