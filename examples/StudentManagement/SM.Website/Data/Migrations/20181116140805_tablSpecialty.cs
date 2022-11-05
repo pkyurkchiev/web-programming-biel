7e849032ba1d97b056a8fcb74647447700b09f5d
Pavel Kyurkchiev <pavkyu@gmail.com>
2022-11-05 18:17:32 +0200

    day 1

---
+++ examples/StudentManagement/SM.Website/Data/Migrations/20181116140805_tablSpecialty.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SM.Website.Data.Migrations
+{
+    public partial class tablSpecialty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Students",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpecialtyId",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Specialtys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialtys", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SpecialtyId",
+                table: "Students",
+                column: "SpecialtyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Specialtys_SpecialtyId",
+                table: "Students",
+                column: "SpecialtyId",
+                principalTable: "Specialtys",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Specialtys_SpecialtyId",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Specialtys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_SpecialtyId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialtyId",
+                table: "Students");
+        }
+    }
+}
---
+++ examples/StudentManagement/SM.Website/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SM.Website.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SM.Website.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SM.Website.Data.ApplicationDbContext _context;
+
+        public IndexModel(SM.Website.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+        public string StudentName { get; set; }
+        public SelectList Specialties { get; set; }
+        public string StudentSpecialty { get; set; }
+
+        public async Task OnGetAsync(string studentName, int? studentSpecialty)
+        {
+            var students = from s in _context.Students select s;
+
+            if (!String.IsNullOrEmpty(studentName))
+                students = students.Where(s => s.FirstName.Contains(studentName));
+
+            if (studentSpecialty.HasValue)
+                students = students.Where(s => s.SpecialtyId == studentSpecialty.Value);
+
+            Student = await students.Include(x => x.Specialty).ToListAsync();
+            StudentName = studentName;
+            Specialties = new SelectList(await _context.Specialtys.ToListAsync(), "Id", "Name");
+        }
+    }
+}
